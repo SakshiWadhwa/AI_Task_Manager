96947bb8e208711fd0cd44c962c9b4bb9e3467ce
Shubham Bansal <shubhambansal@shubhams-air.home>
2025-02-10 12:41:29 -0500

    removed comments

--- task_backend/backend/settings.py
+++ task_backend/backend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    
+
     "rest_framework_simplejwt.token_blacklist",
 
     #third-party libs
@@ -76,17 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "backend.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -97,8 +86,6 @@ DATABASES = {
         'PORT': '5432',  # Default PostgreSQL port
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
